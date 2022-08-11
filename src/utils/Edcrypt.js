9e319f49ab6b79514be6d1084a26e9ed1a5cf202
mufis-coder <muhamadfikrisunandar@gmail.com>
2022-08-11 17:28:42 +0700

    feat: add index css and add encrypt for all char

--- src/utils/Edcrypt.js
+++ src/utils/Edcrypt.js
@@ -1,25 +1,28 @@
 const encrypt = (texIn, password) => {
     var lenPass= password.length, lenTextIn = texIn.length;
-    var iPass = 0;
-    var resText = "";
+    var iPass = 0, resText = "", numAdd = 0;
 
     for (var i=0; i<lenPass; i++) {
 		iPass = parseInt(iPass) + parseInt(password.charAt(i).charCodeAt(0));
 	}
 
+    var lenTextLoop = lenTextIn;
     for(var i=0; i<lenTextIn; i++) {
         if (texIn.charAt(i) === ' '|| texIn.charAt(i) === '\0' || texIn.charAt(i).charCodeAt(0)>123 ||
                 (texIn.charAt(i).charCodeAt(0)<97 && texIn.charAt(i).charCodeAt(0)<65) || 
                 (texIn.charAt(i).charCodeAt(0)<97 && texIn.charAt(i).charCodeAt(0)>91)) {
 			resText = resText.concat(texIn.charAt(i));
 		} else if(texIn.charAt(i).charCodeAt(0)>=65 && texIn.charAt(i).charCodeAt(0)<=90){
-            var tempNum = ((((iPass%(i+1)) + (lenTextIn+1))*lenPass));
-			var tempChar= (tempNum + texIn.charAt(i).charCodeAt(0))%26 + 65;
-			resText = resText.concat(String.fromCharCode(tempChar));
+            var tempNum = ((((iPass%(i+1)) + (lenTextLoop+1))*lenPass));
+            var tempChar= (tempNum + texIn.charAt(i).charCodeAt(0))%26 + 65;
+            resText = resText.concat(String.fromCharCode(tempChar));
         } else{
-
-            resText = resText.concat(texIn.charAt(i));
+            var tempNum = ((((iPass%(i+1)) + (lenTextLoop+1))*lenPass));
+            var tempChar= (tempNum + texIn.charAt(i).charCodeAt(0))%26 + 97;
+            resText = resText.concat(String.fromCharCode(tempChar));
         }
+        
+        lenTextLoop -= 1;
     }
 
     return (resText);
