7d72a4ce81d4e0dcfc655decc0053769c7293e79
mufis-coder <muhamadfikrisunandar@gmail.com>
2022-08-15 08:52:07 +0700

    feat: add decrypt algorithm

--- src/utils/Edcrypt.js
+++ src/utils/Edcrypt.js
@@ -29,7 +29,51 @@ const encrypt = (texIn, password) => {
 }
 
 const decrypt = (texIn, password) => {
-    return ('str');
+    var lenPass= password.length, lenTextIn = texIn.length;
+    var iPass = 0, resText = "";
+
+    for (var i=0; i<lenPass; i++) {
+		iPass = parseInt(iPass) + parseInt(password.charAt(i).charCodeAt(0));
+	}
+
+    var lenTextLoop = lenTextIn;
+    for(i=0; i<lenTextIn; i++) {
+        if (texIn.charAt(i) === ' '|| texIn.charAt(i) === '\0' || texIn.charAt(i).charCodeAt(0)>123 ||
+                (texIn.charAt(i).charCodeAt(0)<97 && texIn.charAt(i).charCodeAt(0)<65) || 
+                (texIn.charAt(i).charCodeAt(0)<97 && texIn.charAt(i).charCodeAt(0)>91)) {
+			resText = resText.concat(texIn.charAt(i));
+		} else if(texIn.charAt(i).charCodeAt(0)>=65 && texIn.charAt(i).charCodeAt(0)<=90){
+            var charAt = texIn.charAt(i).charCodeAt(0) - 65;
+            var tempNum = ((((iPass%(i+1)) + (lenTextLoop+1))*lenPass));
+            var tempChar= 0 + charAt - tempNum;
+            while(true){
+                if(tempChar<=90 && tempChar>=65) {
+					break;
+				}
+				else {
+					tempChar += 26;
+				}
+            }
+            resText = resText.concat(String.fromCharCode(tempChar));
+        } else{
+            var charAt = texIn.charAt(i).charCodeAt(0) - 97;
+            var tempNum = ((((iPass%(i+1)) + (lenTextLoop+1))*lenPass));
+            var tempChar= 0 + charAt - tempNum;
+            while(true){
+                if(tempChar<=122 && tempChar>=97) {
+					break;
+				}
+				else {
+					tempChar += 26;
+				}
+            }
+            resText = resText.concat(String.fromCharCode(tempChar));
+        }
+        
+        lenTextLoop -= 1;
+    }
+
+    return (resText);
 }
 
 module.exports = {encrypt, decrypt};
