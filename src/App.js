456f238180141b567de01bc5618e2e51d6bdefa0
mufis-coder <muhamadfikrisunandar@gmail.com>
2022-08-18 16:13:05 +0700

    feat: new design

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Content from "./components/Content";
 function App() {
   return (
     <div className="App">
-      <h1>Encrypt & Decrypt App</h1>
       <Content/>
     </div>
   );
--- src/components/Content.js
+++ src/components/Content.js
@@ -17,40 +17,88 @@ const Content = () => {
 
     return ( 
         <div className="content">
-            <form>
-                <label className="control-label">
-                    encrypt
-                    <input type="radio" id="encrypt" value="encrypt" 
-                        name="type-algo" checked={isEncrypt} onChange={handleTypeAlgo}/>
-                </label>
-                
-                <label className="control-label">
-                    decrypt
-                    <input type="radio" id="decrypt" value="decrypt" 
-                        name="type-algo" onChange={handleTypeAlgo}/>
-                </label>
-
-                <label>Input password:</label>
-                <textarea
-                    className="password-area"
-                    required
-                    value={password}
-                    onChange={(e)=>setPassword(e.target.value)}
-                ></textarea>
-
-                <label>Input text:</label>
-                <textarea
-                    className="text-input-area"
-                    required
-                    value={textIn}
-                    onChange={(e)=>setTextIn(e.target.value)}
-                ></textarea>
-                
-                <label>Result:</label>
+        <div className="container">
+        <div className="row justify-content-center">
+        <div className="col-md-10">
+        <div className="row align-items-center">
+            <div className="col-lg-7 mb-5 mb-lg-0">
+                <h2 className="mb-5">A Simple Encrypt <br /> and Decrypt App</h2>
+                <form className="border-right pr-5 mb-5" method="post" id="contactForm" name="contactForm">
+                    <div className="row">
+                        <div className="col-md-12 form-group">
+                            <div className="form-check form-check-inline">
+                                <input 
+                                    className="form-check-input" 
+                                    type="radio" 
+                                    name="inlineRadioOptions" 
+                                    id="inlineRadio1" 
+                                    value="encrypt" 
+                                    checked={isEncrypt} 
+                                    onChange={handleTypeAlgo}/>
+                                <label className="form-check-label" for="inlineRadio1">encrypt</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input 
+                                    className="form-check-input" 
+                                    type="radio" 
+                                    name="inlineRadioOptions" 
+                                    id="inlineRadio1" 
+                                    value="decrypt" 
+                                    onChange={handleTypeAlgo}/>
+                                <label className="form-check-label" for="inlineRadio1">decrypt</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 form-group">
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                required
+                                name="password" 
+                                id="password" 
+                                placeholder='Password'
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 form-group">
+                            <textarea 
+                                className="form-control" 
+                                name="message" 
+                                id="message" 
+                                cols="30" 
+                                rows="5" 
+                                placeholder="Write your message"
+                                required
+                                value={textIn}
+                                onChange={(e)=>setTextIn(e.target.value)}>
+                            </textarea>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 form-group">
+                            <label>Result:</label>
+                            <p id="result-paragraph">
+                                {isEncrypt? encrypt(textIn, password): decrypt(textIn, password)}
+                            </p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-4 ml-auto">
+                <h3 class="mb-4">Edecrypt Web App</h3>
                 <p>
-                    {isEncrypt? encrypt(textIn, password): decrypt(textIn, password)}
+                    Web application to encrypt and decrypt your messages. 
+                    If you have any questions about the algorithm used. Please.
                 </p>
-            </form>
+                <p><a href="https://www.linkedin.com/in/fikris73/" target="_blank">Contact me</a></p>
+            </div>
+        </div>
+        </div>
+        </div>
+        </div>
         </div>
      );
 }
